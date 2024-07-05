d7fd9ddce5528070d6cf65562edd162d5ccae29b
robert <hoangphuongdtvt288@gmail.com>
2024-07-05 17:49:11 +0700

    update

--- spark/08-source-and-sink/source_and_sink.py
+++ spark/08-source-and-sink/source_and_sink.py
@@ -1,8 +1,7 @@
 import os
 
-import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col
+from pyspark.sql.functions import spark_partition_id
 
 import util.config as conf
 from util.logger import Log4j
@@ -19,55 +18,31 @@ if __name__ == '__main__':
 
     log = Log4j(spark)
 
-    # flight_time_df = spark.read.parquet("/data/source-and-sink/flight-time.parquet")
-    #
-    # flight_time_df.printSchema()
-    #
-    # flight_time_df.show()
-    #
-    # log.info(f"Num Partitions before: {flight_time_df.rdd.getNumPartitions()}")
-    # flight_time_df.groupBy(spark_partition_id()).count().show()
-    #
-    # partitioned_df = flight_time_df.repartition(5)
-    # log.info(f"Num Partitions after: {partitioned_df.rdd.getNumPartitions()}")
-    # partitioned_df.groupBy(spark_partition_id()).count().show()
-    #
-    # partitioned_df.write \
-    #     .format("avro") \
-    #     .mode("overwrite") \
-    #     .option("path", "/data/sink/avro/") \
-    #     .save()
-    #
-    # flight_time_df.write \
-    #     .format("json") \
-    #     .mode("overwrite") \
-    #     .partitionBy("OP_CARRIER", "ORIGIN") \
-    #     .option("path", "/data/sink/json/") \
-    #     .option("maxRecordsPerFile", 10000) \
-    #     .save()
+    flight_time_df = spark.read.parquet("/data/source-and-sink/flight-time.parquet")
 
-    flight_time_df = spark.read.json(path="/data/sink/json/")
     flight_time_df.printSchema()
 
     flight_time_df.show()
 
-    flight_time_df.filter(col("CANCELLED") == 1) \
-        .withColumn("FL_DATE", f.to_date("FL_DATE", "yyyy-MM-dd")) \
-        .withColumn("YEAR", f.year("FL_DATE")) \
-        .filter(col("YEAR") == 2000) \
-        .filter(col("DEST") == "ATL") \
-        .select("DEST", "DEST_CITY_NAME", "FL_DATE", "ORIGIN", "ORIGIN_CITY_NAME", "CANCELLED") \
-        .orderBy("FL_DATE") \
-        .show()
+    log.info(f"Num Partitions before: {flight_time_df.rdd.getNumPartitions()}")
+    flight_time_df.groupBy(spark_partition_id()).count().show()
 
-    flight_time_avro = spark.read.format("avro").option("path", "/data/sink/avro/").load()
+    partitioned_df = flight_time_df.repartition(5)
+    log.info(f"Num Partitions after: {partitioned_df.rdd.getNumPartitions()}")
+    partitioned_df.groupBy(spark_partition_id()).count().show()
 
-    flight_time_avro.filter(col("CANCELLED") == 1) \
-        .withColumn("FL_DATE", f.to_date("FL_DATE", "yyyy-MM-dd")) \
-        .withColumn("YEAR", f.year("FL_DATE")) \
-        .groupBy("OP_CARRIER", "ORIGIN") \
-        .agg(f.count("*").alias("NUM_CANCELLED_FLIGHT")) \
-        .orderBy("OP_CARRIER", "ORIGIN") \
-        .show()
+    partitioned_df.write \
+        .format("avro") \
+        .mode("overwrite") \
+        .option("path", "/data/sink/avro/") \
+        .save()
+
+    flight_time_df.write \
+        .format("json") \
+        .mode("overwrite") \
+        .partitionBy("OP_CARRIER", "ORIGIN") \
+        .option("path", "/data/sink/json/") \
+        .option("maxRecordsPerFile", 10000) \
+        .save()
 
     spark.stop()
