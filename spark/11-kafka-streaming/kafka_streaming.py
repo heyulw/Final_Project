1a57f10da50011d8905892083906e1d758a76171
robert <hoangphuongdtvt288@gmail.com>
2024-07-09 14:28:14 +0700

    update

--- spark/11-kafka-streaming/kafka_streaming.py
+++ spark/11-kafka-streaming/kafka_streaming.py
@@ -1,7 +1,7 @@
 import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col
-from pyspark.sql.types import StringType, StructType, StructField, LongType
+from pyspark.sql.types import StringType, StructType, StructField, LongType, ArrayType, MapType
 
 from util.config import Config
 from util.logger import Log4j
@@ -26,33 +26,8 @@ if __name__ == '__main__':
         .load()
 
     df.printSchema()
-    #
-    # x = df.select(col("value").cast(StringType()).alias("value")) \
-    # .
-    schema = StructType([
-        StructField("id", StringType(), True),
-        StructField("time_stamp", LongType(), True),
-        StructField("ip", StringType(), True),
-        StructField("user_agent", StringType(), True),
-        StructField("resolution", StringType(), True),
-        StructField("device_id", StringType(), True),
-        StructField("api_version", StringType(), True),
-        StructField("store_id", StringType(), True),
-        StructField("local_time", StringType(), True),
-        StructField("show_recommendation", StringType(), True),
-        StructField("current_url", StringType(), True),
-        StructField("referrer_url", StringType(), True),
-        StructField("email_address", StringType(), True),
-        StructField("collection", StringType(), True),
-        StructField("product_id", StringType(), True),
-    ])
 
-    json_df = df.select(col("value").cast(StringType()).alias("value")) \
-        .select(f.from_json("value", schema).alias("json"))
-
-    json_df.printSchema()
-
-    query = json_df.select("json.*") \
+    query = df.select(col("value").cast(StringType()).alias("value")) \
         .writeStream \
         .format("console") \
         .option("truncate", False) \
